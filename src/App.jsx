06c08ff74e25653e57b6bdc1f3bb475f0376b300
Sunny Singh <sunnysingh@Sunnys-MacBook-Air.local>
2024-12-22 20:26:51 +0530

    Made login and signup form page with authentication from backend and setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import LandingPage from './Pages/LandingPage/LandingPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import AuthPage from "./Pages/AuthPage/AuthPage";
+import HomePage from "./Pages/HomePage/HomePage";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
   return (
     <div>
-      <LandingPage />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster toastOptions={{duration: 2000}}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import styles from "./Navbar.module.css";
 import logoTitleImage from "../../Pages/LandingPage/assets/LogoTitleSVG.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const redirectToAuthPage = () => {
+        navigate("/auth")
+    }
   return (
     <div className={styles.navBar}>
           <div className={styles.title}>
@@ -9,7 +15,7 @@ const Navbar = () => {
             <p>FormBot</p>
           </div>
           <div className={styles.btn}>
-            <button className={styles.signInBtn} >Sign in</button>
+            <button onClick={redirectToAuthPage} className={styles.signInBtn} >Sign in</button>
             <button className={styles.createBtn} >Create a FormBot</button>
           </div>
         </div>
