282af7f729a9091c43ab9f0ded8b9db33f9ebe07
Sunny Singh <sunnysingh@Sunnys-MacBook-Air.local>
2024-12-29 00:19:43 +0530

    Setup router for Pages and create auth slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
-import AuthPage from "./Pages/AuthPage/AuthPage";
-import HomePage from "./Pages/HomePage/HomePage";
 import { Toaster } from 'react-hot-toast';
+import SignUpPage from "./Pages/SignUpPage/SignUpPage";
+import SignInPage from "./Pages/SignInPage/SignInPage";
+import SharedFormPage from "./Pages/SharedForm/SharedFormPage";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster toastOptions={{duration: 2000}}/>
-    </div>
+    <>
+      <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<LandingPage />}></Route>
+            <Route path="/signup" element={<SignUpPage />}></Route>
+            <Route path="/signin" element={<SignInPage />}></Route>
+            <Route path="/share/:sharedLink" element={<SharedFormPage />} />
+            {/* PROTECTED ROUTES */}
+            <Route
+              path="/dashboard/:userID/*"
+              element={<ProtectedRoute element={DashboardLayout} />}
+            >
+              <Route index element={<WorkSpace />} />
+              <Route
+                path="workspacetool/:folderId?/flow/:formId?/"
+                element={<WorkspaceTool />}
+              />
+              <Route
+                path="workspacetool/:folderId?/theme/:formId?/"
+                element={<Theme />}
+              />
+              <Route
+                path="workspacetool/:folderId?/response/:formId?/"
+                element={<Analytics />}
+              />
+              <Route path="settings" element={<SettingPage />} />
+            </Route>
+            <Route path="*" element={<p>Page Not Found</p>} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster position="top-right" reverseOrder={false} />
+    </>
   );
 }
 
