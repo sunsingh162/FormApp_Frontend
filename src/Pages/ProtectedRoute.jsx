bed5c74f4fb8ff25bcab999e49b2a1a0edeb61ff
Sunny Singh <sunnysingh@Sunnys-MacBook-Air.local>
2024-12-29 22:48:07 +0530

    Added a protected route for redirecting to dashboard when authenticated & Implemented dashboard layout

---
+++ src/Pages/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ element: Component }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? <Component /> : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
--- src/data/apiUrl.js
+++ src/data/apiUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:3000/";
 
 const registerUserURL = `${BASE_URL}user/signup`;
 const loginUserURL = `${BASE_URL}user/login`;
