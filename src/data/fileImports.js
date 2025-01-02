a6520f2bdc916486e9578f2b5e35db59351d9ac2
Sunny Singh <sunnysingh@Sunnys-MacBook-Air.local>
2025-01-02 16:39:46 +0530

    Analytics of shared form with react pie chat completion rate

--- src/data/fileImports.js
+++ src/data/fileImports.js
@@ -7,37 +7,6 @@ import hero_image from "/assets/Hero_section/hero_img.png";
 import her_left_obj from "/assets/Hero_section/hero_left_obj.svg";
 import her_right_obj from "/assets/Hero_section/hero_right_obj.svg";
 
-// // *Import Form_section assets
-// import formSection_form from "/assets/Form_section/form_section_form.png";
-// import form_section_sign_left from "/assets/Form_section/form_section_cross_mark_icon.svg";
-// import form_section_sign_right from "/assets/Form_section/form_section_right_mark_icon.svg";
-// import form_image from "/assets/Form_section/form_image.png";
-// import form_gif from "/assets/Form_section/form_section_gif.gif";
-// import form_hi_image from "/assets/Form_section/form_section_hi_image.svg";
-// import form_arrow_mark from "/assets/Form_section/form_section_arrow_icon.svg";
-
-// *message channel
-// import chat_screenshots from "/assets/LandingPage/typebot-standard.png";
-
-// // *features
-// import calculator_icon from "/assets/LandingPage/feature_calculator.svg";
-// import domain_icon from "/assets/LandingPage/feature_domain.svg";
-// import folder_icon from "/assets/LandingPage/feature_folder.svg";
-// import hidden_icon from "/assets/LandingPage/feature_hidden.svg";
-// import link_icon from "/assets/LandingPage/feature_link.svg";
-// import team_icon from "/assets/LandingPage/feature_team.svg";
-
-// *Teams
-
-// import team_1 from "/assets/LandingPage/team_1.svg";
-// import team_2 from "/assets/LandingPage/team_2.svg";
-// import team_3 from "/assets/LandingPage/team_3.svg";
-// import team_4 from "/assets/LandingPage/team_4.svg";
-// import team_5 from "/assets/LandingPage/team_5.svg";
-// import team_6 from "/assets/LandingPage/team_6.svg";
-// import team_7 from "/assets/LandingPage/team_7.svg";
-// import team_8 from "/assets/LandingPage/team_9.svg";
-
 // *Import SignIn Icons
 import arrow_back from "/assets/Signin/arrow_back.svg";
 
@@ -59,18 +28,6 @@ import phone_input_icon from "/assets/icons/number_input_icon.svg";
 import rating_input_icon from "/assets/icons/number_input_icon.svg";
 import text_input_icon from "/assets/icons/number_input_icon.svg";
 
-//* Themes
-// Big Images
-// import Dark_theme from "/assets/Themes/Dark_theme.jpg";
-// import Light_theme from "/assets/Themes/Light_theme.jpg";
-// import Tail_blue from "/assets/Themes/Tail_blue.jpg";
-
-//* small images
-// import tail_blue_theme from "/assets/Themes/Tail_Blue.svg";
-// import light_theme from "/assets/Themes/light.svg";
-// import dark_theme from "/assets/Themes/dark_theme.png";
-// import hi from "/assets/Themes/hi.svg";
-// import hello from "/assets/Themes/hello.svg";
 import icon from "/assets/Themes/icon.png";
 
 export {
